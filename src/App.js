791d457096c1ad897697fae19bf1baf9b328eb07
nazim <nazimaitali88@gmail.com>
2020-09-04 23:09:13 +0100

    add bootstrap in src folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap/dist/css/bootstrap.css';
   
   class App extends Component {
     render() {
